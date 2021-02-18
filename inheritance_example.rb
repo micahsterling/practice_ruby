518ab7f5f5c7ad91d3c574d2317e6ff779447c06
Micah Sterling <msterling89@gmail.com>
2021-02-17 19:00:03 -0600

    inheritance for car and bike with new class

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,5 +1,5 @@
-class Car
-  def initialize
+class Transportation
+  def initialize(speed, direction)
     @speed = 0
     @direction = 'north'
   end
@@ -15,31 +15,29 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
+
+
+class Car < Transportation
 
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = 'north'
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
+class Bike < Transportation
 
   def ring_bell
     puts "Ring ring!"
   end
 end
+
+car = Car.new(20,'north')
+p car.accelerate
+car.honk_horn
+
+
+bike = Bike.new(10, 'north')
+p bike.accelerate
+bike.ring_bell
+bike.honk_horn
